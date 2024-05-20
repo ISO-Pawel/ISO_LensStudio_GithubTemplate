279e29628ac86b5f1c8c7a1890b77b406af4e865
Pawel Kudel <pawel.kudel@isodesign.co.uk>
2024-05-20 14:44:51 +0100

    Complete code

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import {
+    bootstrapCameraKit,
+    createMediaStreamSource,
+    Transform2D,
+} from '@snap/camera-kit'
+
+(async function(){
+const cameraKit = await bootstrapCameraKit({apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzA5ODE3NTQ1LCJzdWIiOiI3MjE0YjdjNi0zZTIzLTRhOWMtOTkwMS0xNjI4YjUxOGNiMGV-U1RBR0lOR341YmYwZDhhNy1jZjhjLTRjMmMtYjgyNS0zNDY1YjY2MjAyOTcifQ.N631Sj7y6GckzndOhaDB87gbC0vx8DCMSk2ci_24Aoo'});
+
+const session = cameraKit.createSession()
+document.getElementById('canvas').replaceWith(session.output.live)
+
+const {lenses} = await cameraKit.lensRepository.loadLensGroups(['ecfa18a7-3d4b-42e0-8535-c1c3af0ef8a4'])
+
+session.applyLens(lenses[0])
+let mediaStream = await navigator.mediaDevices.getUserMedia({video: 
+    {facingMode: 'environment'}
+})
+
+const source = createMediaStreamSource(mediaStream, {
+    cameraType: 'back'
+})
+
+await session.setSource(source)
+
+session.source.setRenderSize(window.innerWidth, window.innerHeight)
+
+(await session).play()
+
+})();
+
